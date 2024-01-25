3e8548dc6d6c7df48ea21efac7f29beb76ee0704
smuthya <srinathm85@gmail.com>
2024-01-25 15:50:46 -0800

    chore: fix lint issues

--- src/electrode-server.ts
+++ src/electrode-server.ts
@@ -392,4 +392,4 @@ export async function electrodeServer<TConfig = any>(
   return await start(ctx);
 }
 
-export { default as fastify } from 'fastify';
+export { default as fastify } from "fastify";
